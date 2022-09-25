a43001958f98f889f319efad1c2f341c08f494e7
Christoph Halang <bestimmaa@users.noreply.github.com>
2022-09-25 23:43:55 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import Door from "./Door.svelte";
+  let doors = Array.from({ length: 24 }, (_, i) => {
+    return {
+      number: i + 1,
+      content: "Hello World"
+    };
+  });
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  .container {
+    display: grid;
+    grid-template-columns: 200px 200px 200px 200px 200px 200px;
+    grid-template-rows: auto;
+    column-gap: 10pt;
+    row-gap: 10pt;
+    justify-content: center;
+  }
+  .item {
+    grid-column-start: auto;
+    grid-column-end: auto;
+    grid-row-start: auto;
+    grid-row-end: auto;
+  }
+
+  @media only screen and (max-width: 1090px) {
+    .container {
+      grid-template-columns: 200px 200px 200px 200px 200px;
+    }
+  }
+
+  @media only screen and (max-width: 880px) {
+    .container {
+      grid-template-columns: 200px 200px 200px 200px;
+    }
+  }
+  @media only screen and (max-width: 670px) {
+    .container {
+      grid-template-columns: 200px 200px 200px;
+    }
+  }
+  @media only screen and (max-width: 460px) {
+    .container {
+      grid-template-columns: 200px 200px;
+    }
+  }
+  @media only screen and (max-width: 250px) {
+    .container {
+      grid-template-columns: 200px;
+    }
+  }
+</style>
+
+<main>
+	<h1>Simple AdvCal</h1>
+  <div class='container'>
+    {#each doors as door}
+    <Door class='item' suprise={door.content}>{door.number}</Door>
+    {/each}
+  </div>
+</main>
---
+++ Door.svelte
@@ -0,0 +1,44 @@
+<style>
+  .backDoor {
+    background-color: #333;
+    position: relative;
+    width: 200px;
+    height: 300px;
+
+    margin: 0 auto;
+    margin-top: 0px;
+  }
+
+  .door {
+    background-color: brown;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 200px;
+    height: 300px;
+
+    transform-origin: left;
+    /*Speed of the Door animation*/
+    transition: all 0.5s ease-in-out;
+  }
+
+  .doorOpen {
+    /*prespectiv creates the door open effect*/
+    transform: perspective(1200px) translateZ(0px) translateX(0px) translateY(0px)
+      rotateY(-105deg);
+  }
+
+  .number {
+    font-size: 50pt;
+  }
+</style>
+
+<script>
+	let open = false;
+	export let suprise;
+</script>
+
+<div class="backDoor">
+  {suprise}
+	<div class="{open === true ? 'door doorOpen' : 'door'}" on:click="{() => open = !open}"><span class="number"><slot></slot></span></div>
+</div>
